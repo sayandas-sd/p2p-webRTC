d4f22fcd09f8ae8b5322b2116fef0462e5598f65
sayandas-sd <sayandasmac@gmail.com>
2024-09-18 23:22:25 +0530

    added createoffer and receiver ws connection

--- client-side/src/components/Receiver.tsx
+++ client-side/src/components/Receiver.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 export const Receiver = ()=>{
+
+    useEffect(()=>{
+        const socket = new WebSocket('ws://localhost:8080');
+        socket.onopen = ()=>{
+            socket.send(JSON.stringify({
+                type: 'receiver' 
+            }));
+        }
+
+       
+    },[]);
+
     return <div>
         Receiver
     </div>
--- client-side/src/components/Sender.tsx
+++ client-side/src/components/Sender.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react"
+
 export const Sender = ()=>{
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+
+    useEffect(()=>{
+        const socket = new WebSocket('ws://localhost:8080');
+        socket.onopen = ()=>{
+            socket.send(JSON.stringify({type: 'sender' }));
+        }
+    },[])
+
+    async function sendVideo() {
+        if(!socket) return;
+
+        const pc = new RTCPeerConnection();
+        const offer = await pc.createOffer();
+        await pc.setLocalDescription(offer);
+        socket?.send(JSON.stringify({type: 'createOffer', sdp: pc.localDescription}));
+
+    }
+
     return <div>
-        Sender
+        <button
+        onClick={sendVideo}
+        >
+            open camera
+        </button>
     </div>
 }
