030bef94f27902682da230205bd80f5e07225a3b
sayandas-sd <sayandasmac@gmail.com>
2024-09-18 23:24:02 +0530

    added receiver createAnswer

--- client-side/src/components/Receiver.tsx
+++ client-side/src/components/Receiver.tsx
@@ -10,7 +10,17 @@ export const Receiver = ()=>{
             }));
         }
 
-       
+        socket.onmessage = async (event) =>{
+            const message = JSON.parse(event.data);
+            if(message.type === 'createOffer'){
+                const pc = new RTCPeerConnection();
+                pc.setRemoteDescription(message.sdp);
+                const answer = await pc.createAnswer();
+                await pc.setLocalDescription(answer)
+                socket.send(JSON.stringify({type: 'createAnswer', sdp: pc.localDescription}))
+
+            }
+        }
     },[]);
 
     return <div>
